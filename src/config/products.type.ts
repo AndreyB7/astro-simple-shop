82b37e17f7df3ee0381d63f4693bcc48b519c72e
AndreyB7 <0129507@gmail.com>
2025-05-25 07:02:09 +0300

    slug + new products need images

--- src/config/products.type.ts
+++ src/config/products.type.ts
@@ -6,10 +6,12 @@ export type Product = {
 	art: string,
 	cat: string,
 	name: string,
+	slug: string,
 	desc: string,
 	img: string,
 	price: number,
 	roll?: number,
+	storage: number,
 	priceroll?: number,
 	quantity?: number,
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -19,13 +19,13 @@ export function countProductTotal(product: Product) {
 
 export const getProductSlug = (product: Product): string => {
 	const transliterationMap: { [key: string]: string } = {
-	    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
-	    'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
-	    'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
-	    'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
-	    'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch',
-	    'ш': 'sh', 'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '',
-	    'э': 'e', 'ю': 'yu', 'я': 'ya'
+		'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd',
+		'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
+        'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+		'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't',
+		'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch',
+		'ш': 'sh', 'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '',
+		'э': 'e', 'ю': 'yu', 'я': 'ya'
 	};
 
 	const transliteratedName = product.name
